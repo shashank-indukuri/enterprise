e4bb556d70c5c144bc8975ce5bab05fc4811b34f
shashank-indukuri <shashank.indukuri05@gmail.com>
2022-05-16 09:44:23 -0400

    Update

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,4 +3,5 @@ INSERT INTO students (stu_id, nm, email) VALUES
             ('11100', 'Dilly Caffery', 'dcaffery1@blogspot.com'),
             ('22200', 'Marysa Alyonov', 'malyonov3@utexas.edu'),
             ('22201', 'Julie Venturoli', 'jventuroli4@marketwatch.com'),
+            ('32201', 'Shashank Venturoli', 'jventuroli4@marketwatch.com'),
             ('22202', 'SFrieda Liddiatt', 'fliddiatt6@yahoo.co.jp');

133a8c85d9ed55969fa8bbdbeffd7ba2c6b1750f
shashank-indukuri <shashank.indukuri05@gmail.com>
2022-05-24 18:58:28 -0400

    Added okta

--- src/main/react-web/src/App.jsx
+++ src/main/react-web/src/App.jsx
@@ -1,15 +1,39 @@
-import { useState } from "react";
-import StudentList from "./component/StudentList";
-import "./App.css";
+import React from "react";
+import { SecureRoute, Security, LoginCallback } from "@okta/okta-react";
+import { OktaAuth, toRelativeUrl } from "@okta/okta-auth-js";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  useHistory,
+} from "react-router-dom";
+import Home from "./Home";
+import Protected from "./Protected";
 
-function App() {
-  const [count, setCount] = useState(0);
+const oktaAuth = new OktaAuth({
+  issuer: "https://trial-1640146.okta.com/oauth2/default",
+  clientId: "0oa168p4r9QWpvKrY697",
+  redirectUri: "http://localhost:3000/login/callback",
+  scopes: ["openid", "profile", "email"],
+  pkce: true,
+  disableHttpsCheck: true,
+});
+
+const App = () => {
+  const history = useHistory();
+  const restoreOriginalUri = async (_oktaAuth, originalUri) => {
+    history.replace(toRelativeUrl(originalUri || "/", window.location.origin));
+  };
 
   return (
-    <div className="App">
-      <StudentList />
-    </div>
+    <Security oktaAuth={oktaAuth} restoreOriginalUri={restoreOriginalUri}>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/login/callback" exact component={LoginCallback} />
+        <SecureRoute path="/protected" component={Protected} />
+      </Switch>
+    </Security>
   );
-}
+};
 
 export default App;
